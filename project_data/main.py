b4eb3761d45c4e685785b011dbc948cd1c537bfb
Karen Poghosyan <karenpoghosyan242@gmail.com>
2025-04-09 21:51:59 +0400

    Added def (list_records and get_record and _save) in class MiniDatabase

--- project_data/main.py
+++ project_data/main.py
@@ -11,26 +11,33 @@ class MiniDatabase:
         except:
             with open(self.file_name, "w") as file:
                 json.dump([], file)
-
+    def _save(self):
+        with open(self.file_name, "w") as file_w:
+            json.dump(self.data, file_w, indent=4)  
+        
     def add_record(self, record: dict):
         self.data.append(record)
-        with open(self.file_name, "w") as file_w:
-            json.dump(self.data, file_w, indent=4)  #
+        self._save()
 
     def get_record(self, record_id: int) -> dict:
-        pass
-
+        for record in self.data:
+            if record.get('id') == record_id:
+                return record
+        return None
+#
     def update_record(self, record_id: int, new_data: dict):
         pass
 
     def delete_record(self, record_id: int):
-        pass
+        pass   
+#   
     def list_records(self) -> list:
-        pass
+        return self.data
     
     
-db = MiniDatabase()
-record_1 = {"id": 1, "name": "Karen", "age": 16, "email": "karen@example.com"}
-db.add_record(record_1)
-record_2 = {"id": 2, "name": "Anna", "age": 18, "email": "anna@example.com"}
-db.add_record(record_2)
+mini = MiniDatabase()
+record_1 = {"id": 1, "name": "Karen", "age": 15, "email": "karenpoghosyan@gmail.com"}
+mini.add_record(record_1)
+record_2 = {"id": 2, "name": "Anna", "age": "+-12", "email": "ani47556@gmail.com"}
+mini.add_record(record_2)
+print(mini.list_records())
