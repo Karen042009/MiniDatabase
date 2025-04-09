87ba14574a9d189f59e39b56776b62522a5a5f18
Karen Poghosyan <karenpoghosyan242@gmail.com>
2025-04-09 21:38:56 +0400

    Added def add_record in class MiniDatabase

--- project_data/main.py
+++ project_data/main.py
@@ -4,9 +4,18 @@ import json
 class MiniDatabase:
     def __init__(self, file_name="data.json"):
         self.file_name = file_name
+        self.data = []
+        try:
+            with open(self.file_name) as file:
+                self.data = json.load(file)
+        except:
+            with open(self.file_name, "w") as file:
+                json.dump([], file)
 
     def add_record(self, record: dict):
-        pass
+        self.data.append(record)
+        with open(self.file_name, "w") as file_w:
+            json.dump(self.data, file_w, indent=4)  #
 
     def get_record(self, record_id: int) -> dict:
         pass
@@ -16,6 +25,12 @@ class MiniDatabase:
 
     def delete_record(self, record_id: int):
         pass
-
     def list_records(self) -> list:
         pass
+    
+    
+db = MiniDatabase()
+record_1 = {"id": 1, "name": "Karen", "age": 16, "email": "karen@example.com"}
+db.add_record(record_1)
+record_2 = {"id": 2, "name": "Anna", "age": 18, "email": "anna@example.com"}
+db.add_record(record_2)
